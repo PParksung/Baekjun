869a8067e6ad8c106243894802d38b8ae4ef2d58
PParksung <smp0417@naver.com>
2024-02-26 18:25:33 +0900

    UsingSwitchCase

--- 10828_stack/PracticeStack.java
+++ 10828_stack/PracticeStack.java
@@ -1,56 +1,57 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
 import java.util.Stack;
-
 public class PracticeStack {
+    
     public static void main(String[] args) throws IOException {
         
-        Stack <String> stack = new Stack();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        Stack <Integer> stack =  new Stack();
+        StringBuilder sb = new StringBuilder();
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        
-        int n = Integer.parseInt(bf.readLine());
-        
         for(int i=0; i<n; i++){
-            String type = bf.readLine();
-            String arr[] = type.split(" ");
-            String result = "";
-            
-            if(arr[0].equals("push")){
-                result = stack.push(arr[1]);
-            }
-            else if(arr[0].equals("top")){
-                if(stack.isEmpty()){
-                    result = stack.peek();
-                }
-                else{
-                    result = "-1";
-                }
-            }
-            else if(arr[0].equals("size")){
-                result = String.valueOf(stack.size());
-            }
-            else if(arr[0].equals("empty")){
-                result = stack.isEmpty() ? "1" : "0" ;
-            }
-            else if(arr[0].equals("pop")){
-                if(stack.isEmpty()){
-                    result = stack.pop();
-                }
-                else{
-                    result = "-1";
-                }
-            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String type = st.nextToken();
+        
 
-            bw.write(result + "\n");
-
-            
-        } 
-        bw.close();
-    }    
+            switch(type){
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken())); 
+                    break;
+                case "pop":
+                    if(!stack.isEmpty()){
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    else{
+                        sb.append("-1").append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    if(stack.isEmpty()){
+                        sb.append("1").append("\n");
+                    }
+                    else{
+                        sb.append("0").append("\n");
+                    }
+                    break;
+                case "top":
+                    if(!stack.isEmpty()){
+                        sb.append(stack.peek()).append("\n");
+                    }
+                    else{
+                        sb.append("-1").append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
 }
